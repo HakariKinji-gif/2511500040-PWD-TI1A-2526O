44bd6391d10dd262bcf2ba7ff5ee4b966a801c29
Hengky Febrilien <2511500040@mahasiswa.atmaluhur.ac.id>
2025-11-26 11:24:25 +0700

    menambahkan kode baru untuk menampilkan array menggunakan looping foreach

--- pertemuan-09/index.php
+++ pertemuan-09/index.php
@@ -117,9 +117,17 @@ endif;
       "adik" => ["label" => "Nama Adik:", "suffix" => ""]
     ];
     ?>
-    
-    <section id="about">
-      <h2>Tentang Saya</h2>
+
+      <section id="about">
+        <h2>Tentang Saya</h2>
+        <?php foreach ($fieldConfig as $kunci => $metadata): ?>
+          <p> 
+            <strong><?= $metadata["label"] ?></strong>
+            <?= htmlspecialchars(string: $biodata[kunci] ?? "") ?>
+            <?= $metadata["suffix"] ?>
+        </P>
+        <?php endforeach; ?>
+        
       <p><strong>NIM:</strong> <?= $txtNim ?></p>
       <p><strong>Nama Lengkap:</strong> <?= $txtNmLengkap ?> &#128526;</p>
       <p><strong>Tempat Lahir:</strong> <?= $txtT4Lhr ?></p>
