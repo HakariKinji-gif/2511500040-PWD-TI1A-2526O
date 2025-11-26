70f56049e8c1ea2a5e261a9de79e1801225f1a7d
Hengky Febrilien <2511500040@mahasiswa.atmaluhur.ac.id>
2025-11-26 11:57:42 +0700

    mengubah kode di proses.php menjadi array associative

--- pertemuan-09/proses.php
+++ pertemuan-09/proses.php
@@ -1,11 +1,10 @@
 <?php
 session_start();
-$sesnama = $_POST["txtNama"];
-$sesemail = $_POST["txtEmail"];
-$sespesan = $_POST["txtPesan"];
-$_SESSION["sesnama"] = $sesnama;
-$_SESSION["sesemail"] = $sesemail;
-$_SESSION["sespesan"] = $sespesan;
+$arrBiodata = [
+"Nama" => $_POST["txtNm"] ?? "",
+"Email" => $_POST["txtEmail"] ?? "",
+"Pesan" => $_POST["txtPesan"] ?? ""
+];
 
 $arrBiodata = [
 "nim" => $_POST["txtNim"] ?? "",
