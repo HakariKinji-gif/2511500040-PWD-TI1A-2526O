0d36ee61b664c4c0fb54c69ec2e9ad5cc9204e01
Hengky Febrilien <2511500040@mahasiswa.atmaluhur.ac.id>
2025-12-03 12:12:13 +0700

    menambah record untuk tabel tb;_tamu sebanyak 1 record

--- pertemuan-10/read.php
+++ pertemuan-10/read.php
@@ -4,7 +4,7 @@ require 'koneksi.php';
 $sql = "SELECT * FROM tbl_tamu ORDER BY cid DESC";
 $q = mysqli_query($conn, $sql);
 ?>
-<table border="1" cellpadding="10" cellspacing="0">
+<table border="1" cellpadding="8" cellspacing="0">
     <tr>
         <th>ID</th>
         <th>Nama</th>
@@ -12,12 +12,12 @@ $q = mysqli_query($conn, $sql);
         <th>Pesan</th>
     </tr>
 
-    <?php while ($r = mysqli_fetch_assoc($q)) : ?>
+    <?php while ($row = mysqli_fetch_assoc($q)) : ?>
         <tr>
             <td><?= $row['cid']; ?></td>
             <td><?= htmlspecialchars($row['cnama']); ?></td>
-            <td><?= htmlspecialchars($row['cmail']); ?></td>
-            <td><?= n12br(htmlspecialchars($row['cpesan'])); ?></td>
+            <td><?= htmlspecialchars($row['cemail']); ?></td>
+            <td><?= nl2br(htmlspecialchars($row['cpesan'])); ?></td>
         </tr>
         <?php endwhile; ?>
       </table>
