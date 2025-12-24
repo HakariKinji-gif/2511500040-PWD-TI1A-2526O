1e04d911ebdeb80518e421162ff604b56bde29c8
Hengky Febrilien <2511500040@mahasiswa.atmaluhur.ac.id>
2025-12-24 11:24:41 +0700

    Menambah link delete untuk file proses_update.php

--- pertemuan-13/README.md
+++ pertemuan-13/README.md
@@ -4,3 +4,5 @@ Hari ini, Mapel PWD pertemuan terakhir Rabu 24 Desember 2025<br>
 Nama saya Hengky Febrilien<br>
 <ol>
 <li> mengimport SQL Bahan pertemuan 13</li>
+<li> Menambah link delete untuk file proses_update.php</li>
+</ol>
--- pertemuan-13/edit.php
+++ pertemuan-13/edit.php
@@ -106,7 +106,7 @@
         <?php endif; ?>
         <form action="proses_update.php" method="POST">
 
-          <input type="text" name="cid" value="<?= (int)$cid; ?>">
+          <input type="hidden" name="cid" value="<?= (int)$cid; ?>">
 
           <label for="txtNama"><span>Nama:</span>
             <input type="text" id="txtNama" name="txtNamaEd" 
